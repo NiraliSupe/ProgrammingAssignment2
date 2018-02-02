48e2c119531b5c4a2ff3ab9f07a6eb3e488123a8
NiraliSupeVT <nsupe@gcmfx.com>
2018-02-02 15:32:31 -0600

    Tested the code and fixed typo

--- cachematrix.R
+++ cachematrix.R
@@ -2,29 +2,31 @@
 
 ## Takes a matrix and returns a set of functions which allow to 
 ## cache inverse calculation.
-## It increses the performance ad it saves a lot of time to calculate 
+## It increases the performance ad it saves a lot of time to calculate 
 ## inverse if it's been already proceed once.
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
+  inverse <- NULL
   ## Set the matrx value
   set <- function(y) {
     x <<- y
-    inv <<- NULL
+    inverse <<- NULL
   }
   ## Get the matrix value
   get <- function() x
   ## Set the cache inverse value
-  getinv <- finction() inverse
-  # Return the vector of functions for getting special matrix
+  setinv <- function(inv) inverse <<- inv
+  ## Get the cached inverse value
+  getinv <- function() inverse
+  # Return the vector of functions for getting a special matrix
   list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Returns the inverse from cache from a matric created by makeCacheMatrix
+## Returns the inverse from cache from a matrix created by makeCacheMatrix
 ## Else find the inverse and store in the cache
 
-cacheSolve <- function(x, ...) {
-        ## Check the cache for inverse
+cacheSolve <- function(x) {
+  ## Check the cache for inverse
   inv <- x$getinv()
   ## If found, return it
   if(!is.null(inv)) {
@@ -39,5 +41,5 @@ cacheSolve <- function(x, ...) {
 }
 
 m <- makeCacheMatrix(matrix(rnorm(25),5,5))
-cacheSolve(m)    ## This call calculate the inverse and store in cache
+cacheSolve(m)    ## This call calculates the inverse and store in cache
 cacheSolve(m)    ## The second call retrieves inverse from cache
